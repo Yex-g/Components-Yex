39557147016a1ee36621ae19833b30bf10039e9c
Yex-g <1203847397@qq.com>
2019-12-13 08:07:28 +0800

    Yex
    add new Files

---
+++ react-yex/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+let dom = <h1>你真帅</h1>
+console.log(dom)
+ReactDOM.render(<App/>, document.getElementById('root'))
