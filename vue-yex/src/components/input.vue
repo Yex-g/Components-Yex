6ae1d0ea0ddc2a0f74a487c8e15a7c9c02706c33
Yex-g <1203847397@qq.com>
2020-03-20 14:46:25 +0800

    Updata xmind

---
+++ vue-yex/src/components/input.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <label v-if= "label!= '' ">{{label}}</label>
+        <input :type="type" v-if="type!='textarea'" />
+        <textarea v-else></textarea>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        label:{
+            type:String
+        },
+        icon:{
+            type:String
+        },
+        iconposition:{
+            default:'left',
+            type:String
+        },
+        type:{
+            type:String,
+            default:'text'
+        },
+        maxlength:{
+            type:Number,
+        },
+        minlenght:{
+            type:Number
+        },
+        placeholder:{
+            type:String,
+            default:''
+        },
+        clearable:{
+            type:Boolean,
+            default:false
+        },
+        disabled:{
+            type:Boolean,
+            default:false
+        },
+        readonly:{
+            type:Boolean,
+            default:false,
+        },
+
+    },
+    components: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    watch: {
+
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    },
+};
+</script>
+
+<style>
+
+</style>
