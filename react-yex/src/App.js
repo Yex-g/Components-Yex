c548cf009f2d59de2dfc85d98e225664bfe1cc80
Yex-g <1203847397@qq.com>
2020-06-16 21:14:21 +0800

    update

--- react-yex/src/App.js
+++ react-yex/src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
-import JsxTest from './Component/ComType'
-import Clock from './Component/Clock'
-import ContextTest from './Component/Context'
-import HocTest from './Component/HocTest'
+
+import List from './views/list'
 export default class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            list:[
+                11,22,33
+            ]
+        }
+    }
   render() {
     return (
       <div>
-        <JsxTest />
-        <Clock />
-        <ContextTest></ContextTest>
-        <HocTest />
+          <List lists = {this.state.list}></List>
       </div>
     )
   }
--- react-yex/src/index.js
+++ react-yex/src/index.js
@@ -2,6 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-let dom = <h1>你真帅</h1>
-console.log(dom)
+import './styles/index.scss'
 ReactDOM.render(<App/>, document.getElementById('root'))
